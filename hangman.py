33e970e1bd95abead3ac30cdd00f16249ac518f9
Kyle McConaughey <kylemcconaughey03@gmail.com>
2020-09-12 15:55:09 -0400

    got all my comments now i hafta write code i guess

--- hangman.py
+++ hangman.py
@@ -4,8 +4,27 @@ def hangman():
 
 # def open words.txt, read them into blob
 
+# wrongGuessCount = 8
+
+# alreadyGuessed = []
 
 # def select random word - must take difficulty input
 
 
-#
+# create key:value object with each key: being a letter in the word and the :value being bool true or false
+# create a list with each letter in the word, for displaying purposes
+
+# input = input.upper(), if isalpha().len(1)
+
+# if input in alreadyGuessed, let user know
+    # else alreadyGuessed.append(input)
+
+# if guess is wrong, wrongGuessCount -= 1, display to user
+
+# display function should loop through each letter in the list, and check the object. if true (guessed correctly) display the letter, if false (not yet guessed) display '_' in its place
+
+# display should be run after each guess is computed
+
+# each time display is run, checkIfWonOrLost() should be run - if False not in wordObject keys, player has won. Otherwise check if wrongGuessCount = 0, in which case player has lost. Else, guess again.
+
+# ask if they want to play again - if so, reset wrongGuessCount, alreadyGuessed, wordObject, wordList, etc.
