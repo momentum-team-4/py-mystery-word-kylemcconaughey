4989a2ff3346efce0499029b451cbede80282744
Kyle McConaughey <kylemcconaughey03@gmail.com>
2020-09-12 22:43:40 -0400

    got the unguessed word to display, now i need to actually make a game

--- hangman.py
+++ hangman.py
@@ -20,11 +20,15 @@ def randomWord(difficulty=int(input('Enter the max length of word you would like
         blob = infile.read()
 
     allWords = blob.upper().split()
+    global wordList
     wordList = [x for x in allWords if len(x) <= difficulty]
+    global word
     word = random.choice(wordList)
+    global letterList
     letterList = [x for x in word]
+    global letterDict
     for i in range(len(word)):
-        letterDict[letterList[i]] = False
+        letterDict[i] = False
     print(f'letterList: {letterList}')
     print(f"letterDict: {letterDict}")
     return word
@@ -40,21 +44,44 @@ def randomWord(difficulty=int(input('Enter the max length of word you would like
 # display function should loop through each letter in the list, and check the object. if true (guessed correctly) display the letter, if false (not yet guessed) display '_' in its place
 
 def letterOrDash(word):
+    print(f"47 lOD word: {word}")
+    soFar = ''
     for i in range(len(word)):
-        if letterDict[letterList[i]]:
-            return letterList[i]
+        print(i)
+        if letterDict[i]:
+            soFar += letterList[i] + ' '
         else:
-            return '_'
+            soFar += '_ '
+    return soFar
 
 
 def displayWord(word):
-    print(f"word in displayWord = {word}")
-    print(f"So far you've got: {letterOrDash(word)}")
+    print(f"l57 So far you've got: {letterOrDash(word)}")
 
     # display should be run after each guess is computed
 
     # each time display is run, checkIfWonOrLost() should be run - if False not in wordObject keys, player has won. Otherwise check if wrongGuessCount = 0, in which case player has lost. Else, guess again.
     # ask if they want to play again - if so, reset wrongGuessCount, alreadyGuessed, wordObject, wordList, etc.
+
+
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
 if __name__ == "__main__":
     import argparse
     from pathlib import Path
